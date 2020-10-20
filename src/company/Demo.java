3f4c9acd7832d1ddd305f1466d273672f00ca871
Mortezajan <mortezabbm2015@gmail.com>
2020-10-20 12:16:34 +0700

    this is finall message from usa team

---
+++ src/company/Demo.java
@@ -0,0 +1,12 @@
+package company;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("this the new class inside the gitx2 in demo class");
+
+	}
+
+}
